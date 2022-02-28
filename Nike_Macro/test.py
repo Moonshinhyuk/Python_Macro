341dd32d27389788cf3bbf03667473ce5fcc2e08
Moonshinhyuk <73520866+Moonshinhyuk@users.noreply.github.com>
2022-02-28 16:56:48 +0900

    driver and click update

--- Nike_Macro/test.py
+++ Nike_Macro/test.py
@@ -1,14 +1,68 @@
-import requests
+import chromedriver_autoinstaller
 
-url = 'https://www.nike.com/kr/ko_kr/w/xg/fw/nsw/air-force-1-collection'
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"
-}
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import chromedriver_autoinstaller
+import subprocess
 
-r = requests.get(url, headers=headers)
+subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chrometemp"') # 디버거 크롬 구동
 
 
-print(r)
-print(r.request.headers)
-print(r.text)
+option = Options()
+option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+try:
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
+except:
+    chromedriver_autoinstaller.install(True)
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
+driver.implicitly_wait(10)
+
+
+
+driver.get('https://www.nike.com/kr/ko_kr/')
+
+
+# id = input("아이디를 입력해주세요 : ")  #akdls7900@gmail.com
+# pw = input("비밀번호를 입력해주세요 : ") #Akdls6661@
+
+id = "akdls7900@gmail.com"
+pw = "Akdls6661@"
+
+try:
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'login')))
+    elem_btn = driver.find_element(By.CLASS_NAME, 'login')
+    elem_btn.click() # 버튼 클릭
+
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'j_username')))
+    elem_id = driver.find_element(By.ID, 'j_username')
+    elem_id.send_keys(id) # id 입력
+
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'j_password')))
+    elem_pw = driver.find_element(By.ID, 'j_password')
+    elem_pw.send_keys(pw)
+
+    login_btn = driver.find_element(By.XPATH, '//button[@class="button large width-max"]')
+    login_btn.click()
+except: # 이미 로그인이 되어있는 경우
+    pass
+
+
+WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.LINK_TEXT, "Men")))
+men_btn = driver.find_element(By.LINK_TEXT, "Men")
+men_btn.click()
+
+WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.LINK_TEXT, "신발")))
+men_btn = driver.find_element(By.LINK_TEXT, "신발")
+men_btn.click()
+
+# WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH, "/html/body/section/section/section/article/div/div[2]/ul/li[1]/div/div[1]/a/div[2]/div[1]/img")))
+# shoe_btn = driver.find_element(By.XPATH, "/html/body/section/section/section/article/div/div[2]/ul/li[1]/div/div[1]/a/div[2]/div[1]/img")
+# shoe_btn.click()
+
+a = input()
